14948f3ecd3eed2e6d61d3f6f5858ec89971e1f0
szg7044315 <szg_niit@163.com>
2014-05-26 22:33:08 +0800

    project upload
    project upload pomelo test003

---
+++ src/com/the/best/way/TestJava.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.the.best.way;
+
+/**
+ * @author Administrator
+ *
+ */
+public class TestJava {
+	public static void main(String[] args) {
+		System.out.println("this is my test!");
+	}
+}
