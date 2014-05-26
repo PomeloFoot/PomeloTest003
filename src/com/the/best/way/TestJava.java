cd9626b2701c11619b86b2ff1720120a41beac64
szg7044315 <szg_niit@163.com>
2014-05-26 22:55:26 +0800

    add one sting parameter.

--- src/com/the/best/way/TestJava.java
+++ src/com/the/best/way/TestJava.java
@@ -8,6 +8,9 @@ package com.the.best.way;
  *
  */
 public class TestJava {
+	
+	private String test="abc";
+	
 	public static void main(String[] args) {
 		System.out.println("this is my test!");
 	}
